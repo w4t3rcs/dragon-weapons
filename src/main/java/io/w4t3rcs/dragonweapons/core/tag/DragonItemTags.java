fb1a3406341a3335d8f953fdff6c55a595f4686c
w4t3rcs <w4t3rofficial@gmail.com>
2025-06-01 01:40:23 +0200

    Some small updates

--- src/main/java/io/w4t3rcs/dragonweapons/core/tag/DragonItemTags.java
+++ src/main/java/io/w4t3rcs/dragonweapons/core/tag/DragonItemTags.java
@@ -1,11 +1,9 @@
 package io.w4t3rcs.dragonweapons.core.tag;
 
-import com.temporal.api.core.engine.io.context.InjectionContext;
-import com.temporal.api.core.tag.factory.ItemTagFactory;
+import com.temporal.api.core.util.other.TagUtils;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
 public final class DragonItemTags {
-    private static final ItemTagFactory ITEM_TAG_FACTORY = InjectionContext.getFromInstance(ItemTagFactory.class);
-    public static final TagKey<Item> REPAIRS_DRAGON_ARMOR = ITEM_TAG_FACTORY.createTag("repairs_dragon_armor");
+    public static final TagKey<Item> REPAIRS_DRAGON_ARMOR = TagUtils.createItemTag("repairs_dragon_armor");
 }
